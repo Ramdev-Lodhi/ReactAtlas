6ef31d0bb2c1555c9fbd511c9944edfa43fee142
Ramdev Lodhi <ramdevlodhi9399@gmail.com>
2025-01-01 23:09:10 +0530

    feat: All Page setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import { About } from "./pages/About";
+import { Home } from "./pages/Home";
+import { Country } from "./pages/Country";
+import { Contact } from "./pages/Contact";
+import { AppLayout } from "./components/Layouts/AppLayout";
+import { ErrorPage } from "./pages/ErrorPage";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/country",
+        element: <Country />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <h1>Hello</h1>
-    </>
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
